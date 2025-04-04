ab3b261504388ac5d20ca2d77166422439818367
Maksimka2280 <maksimkalol1402@gmail.com>
2025-04-04 23:31:53 +0300

    fix: resolve merge conflicts

--- src/components/shared/Card/Card.tsx
+++ src/components/shared/Card/Card.tsx
@@ -3,14 +3,14 @@
 import { ArrowLeft, ArrowRight, Heart, Layers2, MapPin, Timer } from 'lucide-react';
 import { useState } from 'react';
 import ReactPaginate from 'react-paginate';
-import { useCurrency } from '../../Context/Contextcurrency/Contextcurrency';
+
 export const Card = () => {
   const [liked, setLiked] = useState(false);
   const [viewed, setViewed] = useState(false);
   const [Layers, setLayers] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
-  const { currencySymbol } = useCurrency();
+
   const images = ['/img/image123.png', '/img/room-test.png'];
 
   // Количество картинок
