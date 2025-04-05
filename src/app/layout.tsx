cc1c9be9aaf8421a0b295238f9823902e8f4358b
Maksimka2280 <maksimkalol1402@gmail.com>
2025-04-05 22:34:41 +0300

    fix(root header): fix564666

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import { ReactNode } from 'react';
 import { fonts } from '@shared/styles/font';
 import { Providers } from '@components';
 import '@shared/styles/globals.scss';
-
 import { CurrencyProvider } from '../components/Context/Contextcurrency/Contextcurrency';
-
 export const metadata: Metadata = {
   title: 'Penguin Dev - Home', // TODO: replace with your own title
   description: 'Penguin Dev - description', // TODO: replace with your own description
@@ -20,7 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={fonts}>
         <main className="bg-[#F3F3F3] h-auto">
-    
           <CurrencyProvider>
             <Providers>{children}</Providers>
           </CurrencyProvider>
